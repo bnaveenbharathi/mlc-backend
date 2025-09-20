cfacde3bb18fc8c9ba3fd98242772c726d335313
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-20 17:44:05 +0530

    change conn

--- config/conn.php
+++ config/conn.php
@@ -1,9 +1,15 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $user = "root";
-    private $pass = "";
-    private $db   = "mlc";
+    private $user = "gnthealt_mlc";
+    private $pass = "gnthealt_mlc";
+    private $db   = "gnthealt_mlc";
+
+    
+    // private $host = "localhost";
+    // private $user = "root";
+    // private $pass = "";
+    // private $db   = "mlc";
     public $conn;
 
     public function connect() {
