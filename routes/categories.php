cd7dfa57b0f2bda80406336daeefc6403386f9b7
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-20 18:02:49 +0530

    update cors

--- routes/categories.php
+++ routes/categories.php
@@ -2,10 +2,10 @@
 // categories.php
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', 0);
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Content-Type: application/json");
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
