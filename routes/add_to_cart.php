2cae97b7fa38cd41681a68631a416e687de5115c
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-21 06:56:57 +0530

    update warning

--- routes/add_to_cart.php
+++ routes/add_to_cart.php
@@ -1,7 +1,6 @@
 <?php
 // Disable warnings/notices to avoid breaking JSON
-error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', 0);
+
 
 // CORS & headers
 header("Access-Control-Allow-Origin: *"); 
